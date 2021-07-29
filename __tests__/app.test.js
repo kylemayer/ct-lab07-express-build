7b7e5d5e34d1d15cb573e2f06ffcd016d69d12be
Kyle Mayer <x@kylemayer.dev>
2021-07-29 08:44:50 -0700

    env setup, test built frog POST - red status

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -7,4 +7,16 @@ describe('demo routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
+
+  it('creates a single frog via POST', async () => {
+    const frogger = { name: 'frogger', color: 'green', size: 'tiny' };
+    const res = await request(app).post('/api/v1/frogs').send(frogger);
+
+    expect(res.body).toEqual({
+      id: '1',
+      ...frogger
+    });
+  });
+
+
 });
