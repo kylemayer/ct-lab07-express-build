eb44d4995aff8a71503979792a1e227e31053b15
Kyle Mayer <x@kylemayer.dev>
2021-07-29 09:14:34 -0700

    setup db

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 import app from './lib/app.js';
 import pool from './lib/utils/pool.js';
 
-const PORT = process.env.PORT || 5050;
+const PORT = process.env.PORT || 7890;
 
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
